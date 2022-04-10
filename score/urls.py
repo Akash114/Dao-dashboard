ab05ebf0f86cf82aab75382453c7e590bbc45719
Akash114 <akash.lamprostech@gmail.com>
2022-04-10 20:07:53 +0530

    initial commit

--- score/urls.py
+++ score/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('dao',views.dao,name='dao'),
     path('set_chart',views.set_chart,name='set_chart'),
-    path('praposal_info',views.proposl_info,name='praposal_info')
+    path('proposal_info',views.proposl_info,name='proposal_info')
 ]
